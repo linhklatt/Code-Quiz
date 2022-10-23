ba8a4691ad0dcf8b13b6c1a89d0b1ef7fffb53ff
linhklatt <112294288+linhklatt@users.noreply.github.com>
2022-10-22 18:55:47 -0700

    created HTML, CSS and JS for first part

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var startBtn = document.querySelector(".startBtn");
+var startSection = document.querySelector(".start-section");
+// Set time for Timer section
+function timer() {}
+
+// Start button function
+startBtn.addEventListener("click", function () {
+  startSection.classList.add("hidden");
+});
