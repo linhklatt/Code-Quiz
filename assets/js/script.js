e0ccb3740cbbaa308b67d3d75b9007c667f7a4cf
linhklatt <112294288+linhklatt@users.noreply.github.com>
2022-10-23 20:56:10 -0700

    replaced classes and ids name, added js for showing quiz questions and high score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,66 @@
-// questions array
-var questions = [
+// created variable array questions for the quiz
+var questionArray = [
   {
     showQuestion: "Commonly used data types do not include:",
-    correctAnswer: "Hyper Text Markup Language",
-    options: ["booleans", "numbers", "strings", "Hyper Text Markup Language"],
+    answers: ["booleans", "numbers", "strings", "Hyper Text Markup Language"],
+    correctAnswer: "Hypertext Markup Language",
   },
   {
     showQuestion: "The condition inside an if/else statement is stored inside:",
-    correctAnswer: "parentheses",
-    options: [
+    answers: [
       "parentheses",
       "curly brackets",
       "quotation marks",
       "square brackets",
     ],
+    correctAnswer: "parentheses",
   },
   {
     showQuestion: "Arrays in JavaScript can be used to store:",
+    answers: ["strings", "arrays", "numbers", "all of the above"],
     correctAnswer: "all of the above",
-    options: ["strings", "arrays", "numbers", "all of the above"],
   },
   {
     showQuestion:
-      "String values must be enclosed within __ when being assigned to variables.",
+      "String values must be enclosed within ________ when being assigned to variables.",
+    answers: ["question marks", "quotes", "square brackets", "parentheses"],
     correctAnswer: "quotes",
-    options: ["question marks", "quotes", "square brackets", "parentheses"],
-  },
-  {
-    showQuestion:
-      "A very useful tool used during development and debugging for printing content to the debugger is:",
-    correctAnswer: "console.log",
-    options: ["for loops", "terminal/bash", "JavaScript", "console.log()"],
   },
 ];
-//
-var startQuizEl = document.querySelector(".start-quiz");
+
+// listed all element required for the functions
+var startQuizEl = document.querySelector(".start");
 var timerEl = document.querySelector(".timer");
-var quizText = document.querySelector(".quiz");
-var quizEl = document.querySelector(".quiz-title");
-var choiceEl = document.querySelector(".answer-buttons");
-var alertUserEl = document.querySelector(".alert-user");
+var questionText = document.querySelector(".question");
+var questionEl = document.querySelector(".questionTitle");
+var choiceEl = document.querySelector(".answerButtons");
+var alertUserEl = document.querySelector(".alertUser");
 var resultEl = document.querySelector(".result");
-var correctAnswerEl = document.querySelector(".correct-answer");
-var viewHighScoreEl = document.querySelector(".view-high-score");
-var userInitialEl = document.querySelector(".user-initial");
+var correctAnswerEl = document.querySelector(".correctAnswer");
+var viewHighscoreEl = document.querySelector(".viewHighscore");
+var userInitialEl = document.querySelector(".userInitial");
 var submitBtnEl = document.querySelector(".submitBtn");
-var savedHighScoreEl = document.querySelector(".saved-high-score");
-var highScoresEl = document.querySelector("#highscores");
-var clearHighScoresEl = document.querySelector("#clear");
-var goBackEl = document.querySelector("#go-back");
+var savedHighScoreEl = document.querySelector(".savedHighScore");
+var highscoresEl = document.querySelector("#highscores");
+var clearHighscoresEl = document.querySelector("#clear");
+var goBackEl = document.querySelector("#goBack");
 
 var questionIndex = 0;
-var timeLeft = 60;
+var timeLeft = 75;
 var correctAnsCount = 0;
 var userInitial = "";
 var timeInterval;
 
-// Set time for Timer section
+// start function
+startQuizEl.addEventListener("click", function () {
+  startQuizEl.style.display = "none";
+  viewHighscoreEl.style.display = "none";
+
+  question();
+  time();
+});
+
+// timer function
 function time() {
   var timeInterval = setInterval(function () {
     timeLeft--;
@@ -64,20 +68,131 @@ function time() {
 
     if (timeLeft <= 0) {
       clearInterval(timeInterval);
-      alertUserEl.textContent = "Quiz Ended";
+      alertUserEl.textContent = "All Done!";
       timerEl.textContent = "Time is stopped";
-      quizEl.style.display = "none";
+      questionEl.style.display = "none";
       resultEl.style.display = "block";
     }
   }, 1000);
 }
 
-// Start button function
-startQuizEl.addEventListener("click", function () {
-  startQuizEl.style.display = "none";
-  viewHighScoreEl.style.display = "none";
+// question function
+function question() {
+  questionEl.style.display = "block";
+  var currentQuestion = questionArray[questionIndex];
+  var titleEl = document.querySelector("#questionTitle");
+  titleEl.textContent = currentQuestion.showQuestion;
 
-  time();
+  choiceEl.innerHTML = "";
+
+  currentQuestion.answers.forEach(function (answer, index) {
+    var answerBtn = document.createElement("button");
+    answerBtn.setAttribute("class", "choice");
+    answerBtn.setAttribute("value", answer);
+    answerBtn.textContent = index + 1 + ". " + answer;
+    choiceEl.appendChild(answerBtn);
+
+    answerBtn.addEventListener("click", btnClick);
+  });
+}
+
+// created button click and alert user
+function btnClick() {
+  if (this.value !== questionArray[questionIndex].correctAnswer) {
+    timeLeft -= 10;
+    alertUserEl.textContent = "Wrong!";
+  } else {
+    alertUserEl.textContent = "Correct!";
+    countCorrectAns();
+  }
+  questionIndex++;
+
+  if (questionIndex === questionArray.length) {
+    timeLeft = 0;
+    alertUserEl.textContent = "Quiz Ended";
+    questionEl.style.display = "none";
+    resultEl.style.display = "block";
+    choiceEl.style.display = "none";
+  } else {
+    question();
+  }
+}
+
+// counting the user corrected answer
+function countCorrectAns() {
+  correctAnsCount++;
+  correctAnswerEl.textContent =
+    "You answered " +
+    correctAnsCount +
+    " out of " +
+    questionArray.length +
+    " correctly!";
+}
+
+// stored user high score
+function highscore(e) {
+  choiceEl.style.display = "none";
+  questionText.style.display = "none";
+  e.preventDefault();
+
+  var storeHighscore = localStorage.getItem("savedHighScore");
+  var scoreCountArray;
+
+  if (storeHighscore === null) {
+    scoreCountArray = [];
+  } else {
+    scoreCountArray = JSON.parse(storeHighscore);
+  }
+
+  correctAnswerEl.textContent = correctAnsCount;
+
+  var saveHighscore = {
+    initial: userInitialEl.value.toUpperCase(),
+    saveCorrectAns: correctAnswerEl.textContent,
+  };
+
+  scoreCountArray.push(saveHighscore);
+
+  var scoreCountString = JSON.stringify(scoreCountArray);
+  localStorage.setItem("highscore", scoreCountString);
+
+  viewHighscore();
+}
+var i = 0;
+
+function viewHighscore() {
+  var storedHighscore = localStorage.getItem("highscore");
+
+  if (storedHighscore === null) {
+    return;
+  }
+
+  var scoreCountObject = JSON.parse(storedHighscore);
+  for (; i < scoreCountObject.length; i++) {
+    var newHighscore = document.createElement("h4");
+    newHighscore.innerHTML =
+      scoreCountObject[i].initial + " - " + scoreCountObject[i].saveCorrectAns;
+    highscoresEl.appendChild(newHighscore);
+  }
+}
+
+clearHighscoresEl.addEventListener("click", function () {
+  localStorage.removeItem("highscores");
+  highscoresEl.textContent = "";
 });
 
-// show the question
+submitBtnEl.addEventListener("click", function (e) {
+  highscore(e);
+  savedHighScoreEl.style.display = "block";
+  resultEl.style.display = "none";
+  viewHighscoreEl.style.display = "block";
+});
+viewHighscoreEl.addEventListener("click", function (e) {
+  viewHighscore(e);
+  savedHighScoreEl.style.display = "block";
+});
+goBackEl.addEventListener("click", function () {
+  savedHighScoreEl.style.display = "none";
+  startQuizEl.style.display = "block";
+  location.reload();
+});
